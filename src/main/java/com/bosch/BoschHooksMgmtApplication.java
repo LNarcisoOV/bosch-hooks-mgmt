27bc85cbf9ca868398e1b92e328d8ca0e16d745d
Leonardo Narciso <LNarcisoOV@gmail.com>
2023-04-06 15:29:16 +0000

    First commit.

---
+++ src/main/java/com/bosch/BoschHooksMgmtApplication.java
@@ -0,0 +1,13 @@
+package com.bosch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoschHooksMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoschHooksMgmtApplication.class, args);
+	}
+
+}
